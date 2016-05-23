e5509587dbcb79d912ff2a5a9c33e99b6d6bf2a8
tsotne.putkaradze <tsotne.putkaradze@ttu.ee>
2016-05-24 01:02:59 +0300

    reports updated and script fixed

--- Synthesis/design_vision_script.tcl
+++ Synthesis/design_vision_script.tcl
@@ -11,7 +11,7 @@ create_clock -name "clk" -period 20 -waveform { 0 10  }  { clk  }
 
 compile -exact_map
 
-uplevel #0 { report_area } >> ../reports/REPORTS.txt
+uplevel #0 { report_area } > ../reports/REPORTS.txt
 
 uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS.txt
 
