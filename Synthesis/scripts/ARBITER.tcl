508f2e6e916f9dfc24fbfe47e93b5caa7e61daa7
tsotne.putkaradze <tsotne.putkaradze@ttu.ee>
2016-05-30 14:59:52 +0300

    reports added: all units, and router_v1

---
+++ Synthesis/scripts/ARBITER.tcl
@@ -0,0 +1,15 @@
+analyze -library WORK -format vhdl {
+/home/INTRA/tsotne.putkaradze/P/workspace/EHA/RTL/Arbiter.vhd
+}
+
+elaborate Arbiter -architecture BEHAVIOR -library DEFAULT
+
+create_clock -name "clk" -period 20 -waveform { 0 10  }  { clk  }
+
+compile -exact_map
+
+uplevel #0 { report_area } > ../reports/REPORTS_ARBITER.txt
+
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS_ARBITER.txt
+
+check_design > ../reports/WARNINGS_ARBITER.txt
---
+++ Synthesis/scripts/LBDR.tcl
@@ -0,0 +1,15 @@
+analyze -library WORK -format vhdl {
+/home/INTRA/tsotne.putkaradze/P/workspace/EHA/RTL/LBDR.vhd
+}
+
+elaborate LBDR -architecture BEHAVIOR -library DEFAULT -parameters "cur_addr_rst = 0, Rxy_rst = 60, Cx_rst = 10, NoC_size = 4"
+
+create_clock -name "clk" -period 20 -waveform { 0 10  }  { clk  }
+
+compile -exact_map
+
+uplevel #0 { report_area } > ../reports/REPORTS_LBDR.txt
+
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS_LBDR.txt
+
+check_design > ../reports/WARNINGS_LBDR.txt
