da75973b8f334db4d4fb7d595ab3785bf8c029e0
tsotne.putkaradze <tsotne.putkaradze@ttu.ee>
2016-06-02 14:10:24 +0300

    script and reports added for all muxes

--- Synthesis/scripts/mux/arbiter_muxes.tcl
+++ Synthesis/scripts/mux/arbiter_muxes.tcl
@@ -4,8 +4,8 @@ analyze -library WORK -format vhdl {
 elaborate MUX_2x1_Arbiter_output -architecture RTL -library DEFAULT
 compile -exact_map
 check_design > ../reports/WARNINGS/WARNINGS_MUX_Arbiter.txt
-uplevel #0 { report_area } > ../reports/REPORTS_MUX_Arbiter.txt
-uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS_MUX_Arbiter.txt
+uplevel #0 { report_area } > ../reports/MUX/area_Arbiter.txt
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/MUX/timing_Arbiter.txt
 
 
 
@@ -15,8 +15,8 @@ analyze -library WORK -format vhdl {
 elaborate MUX_5x1_Arbiter_input -architecture RTL -library DEFAULT
 compile -exact_map
 check_design >> ../reports/WARNINGS/WARNINGS_MUX_Arbiter.txt
-uplevel #0 { report_area } >> ../reports/REPORTS_MUX_Arbiter.txt
-uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS_MUX_Arbiter.txt
+uplevel #0 { report_area } >> ../reports/MUX/area_Arbiter.txt
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/MUX/timing_Arbiter.txt
 
 
 
@@ -26,5 +26,5 @@ analyze -library WORK -format vhdl {
 elaborate MUX_6x1_Arbiter_output -architecture RTL -library DEFAULT
 compile -exact_map
 check_design >> ../reports/WARNINGS/WARNINGS_MUX_Arbiter.txt
-uplevel #0 { report_area } >> ../reports/REPORTS_MUX_Arbiter.txt
-uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/REPORTS_MUX_Arbiter.txt
+uplevel #0 { report_area } >> ../reports/MUX/area_Arbiter.txt
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports/MUX/timing_Arbiter.txt
