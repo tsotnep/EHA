a7f788bc760e3dccc692bffe9edd70e3a5fafc8a
tsotnep <tsotnep@gmail.com>
2017-03-08 15:54:36 +0200

    file name fixed

--- Synthesis/scripts/tmr/tmr_voter_LBDR.tcl
+++ Synthesis/scripts/tmr/tmr_voter_LBDR.tcl
@@ -8,6 +8,6 @@ compile
 
 uplevel #0 { report_area } > ../reports_synopsys/tmr/REPORTS_tmr_vvoter_LBDR.txt
 
-uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports_synopsys/tmr/REPORTS_tmr_vvoter_LBDR.txt
+uplevel #0 { report_timing -path full -delay max -nworst 1 -max_paths 1 -significant_digits 2 -sort_by group } >> ../reports_synopsys/tmr/REPORTS_tmr_voter_LBDR.txt
 
 check_design > ../reports_synopsys/tmr/WARNINGS/WARNINGS_tmr_voter_LBDR.txt
